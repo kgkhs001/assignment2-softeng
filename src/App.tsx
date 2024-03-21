2eb0d685df33ae0b630c46e5016af037d5d584a8
kgkhs001 <kgarg@wpi.edu>
2024-03-21 14:47:32 -0400

    Just fixing some errors

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,6 @@
 import Title from "./title.tsx";
 import './App.css'
 import Intro from './intro.tsx'
-//import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
-import './title.tsx'
 
 function App() {
   // const [count, setCount] = useState(0)
