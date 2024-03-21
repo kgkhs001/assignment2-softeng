daf7dfb16753b108faa80372ddbb99121f7a75c1
kgkhs001 <kgarg@wpi.edu>
2024-03-21 13:26:02 -0400

    Merged main and also edited intro.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
-import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import Title from "/title.tsx";
+import Title from "./title.tsx";
 import './App.css'
 
 function App() {
